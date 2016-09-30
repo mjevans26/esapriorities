c4c409072f50d0f1709b40aec00527f13fcc1be9
mjevans26 <evans.michael26@gmail.com>
2016-09-30 13:43:01 -0400

    Conditional panel for bin definitions

--- inst/listing-workplan/app.R
+++ inst/listing-workplan/app.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(esapriorities)
 library(highcharter)
+library(DT)
 
 data("data_clean")
 data("data_states")
@@ -19,7 +20,12 @@ LPN_timetable$Species <- sapply(1:length(LPN_timetable$Timeframe), function(x,y,
 LPN_timetable$Text <- sapply(1:length(LPN_timetable$Timeframe), function(x,y,z) paste(z$Species.name[which(z$Timeframe == y[x,1] & z$LPN == y[x,2])][1:5],collapse="<br>"), y = LPN_timetable, z = data_clean)
 LPN_timetable$Text <- gsub("<br>NA", "", LPN_timetable$Text)
 
+bin_table <- data.frame("Bin" = c(1, 2, 3, 4, 5),
+           "Description" = c("Highest Priority: Critically Imperiled", "Strong status data available", "New science underway", "Conservation efforts underway", "Limited Data"))
+
+
 ui <- fluidPage(
+#  shinyjs::useShinyjs(),
   h1("Workplan Explorer", align="center"),
   fluidRow(
     column(1, br()),
@@ -31,11 +37,24 @@ ui <- fluidPage(
                   reviews of non-candidate species, and the Listing Priority Number (LPN) of current candidate species. 
                   Mouse over the interactive timeline to see a sample of species scheduled for review in a given 
                   fiscal year. For more details on the 7-year workplan, visit:", a("https://www.fws.gov/endangered/improving_esa/listing_workplan.html")))
-      ),
-    highchartOutput("timeline")
+      )
     ),
     column(1, br())
   ),
+  
+  
+  fluidRow(
+    column(1, br()),
+    column(7,
+      highchartOutput("timeline")
+    ),
+    column(3, conditionalPanel(condition = "input.scale == 'Priority'",
+      h4("Priority Bin Definitions"),
+      tableOutput("btable"))
+    ),
+    column(1, br())
+  ),
+  
   
   fluidRow(
     column(1, br()),
@@ -44,8 +63,9 @@ ui <- fluidPage(
     p("The interactive table below contains data from the full 7-year workplan.  Each row documents a species for which a
       status assessment is pending, and includes information about the type of review action planned, the species' geographic range, 
       and the anticipated fiscal year of status review. Use the sort, search, and filtering tools to explore the data.  
-      The filtering tools at the bottom of each column can be used to select records based on specific criteria. For instance, 
+      The filtering tools can be used to select records based on specific criteria. For instance, 
       finding all pending species in a state, or identifying species with high priority scheduled for review in later years."),
+    numericInput("rows","Records per Page:", 10, min = 10, max = 50, step = 10),
     dataTableOutput("dtable")
     ),
     column(1, br())
@@ -69,10 +89,31 @@ server <- function(input, output) {
       hc_xAxis(title = list(text = "Fiscal Year"))%>%
       hc_yAxis(title = list(text = "Number of Species"))%>%
       hc_legend(title = list(text = sprintf("%s<br><em>(click to hide)</em>",input$scale)), 
-                layout = "vertical", align = "right", verticalAlign = "top", y = 100)
+                layout = "vertical", align = "right", verticalAlign = "top", y = 25, floating = TRUE,
+                borderColor = "black", borderWidth = 1, backgroundColor = "white")%>%
+      hc_exporting(enabled = TRUE)
   })
 
-  output$dtable <- renderDataTable({data_clean[,c(2,3,4,6,7,8,9,10)]})
+  output$dtable <- renderDataTable({
+    the_dat <- data_clean[,c(2,3,4,6,7,8,9,10)]
+    datatable(the_dat, 
+                  rownames = FALSE,
+                  class = "cell-border stripe",
+                  filter = "top",
+                  colnames = c("Species", "Proposed Action", "Lead Office", "Timeframe", "Current Candidate", "Range", "LPN", "Priority Bin"),
+                  extensions = "Buttons",
+                  options = list( dom = "Bfrtip", buttons = c("copy", "csv", "excel", "print"),
+                  pageLength = input$rows)
+      )
+  })
+  
+#  observeEvent(input$scale=="LPN",{
+#    shinyjs::hide(id = "def")
+#  }) 
+  
+  output$btable <- renderTable({bin_table
+    }, rownames = FALSE, digits = 0, align = 'l')
+  
 }
 
 shinyApp(ui = ui, server = server)
