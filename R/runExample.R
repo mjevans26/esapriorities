2c89f4db099901ef0a8ec1790e06425b9617b034
Jacob Malcom <jmalcom@defenders.org>
2017-03-21 06:14:33 -0400

    Fix bad stop message.

--- R/runExample.R
+++ R/runExample.R
@@ -6,7 +6,7 @@
 runExample <- function() {
   appDir <- system.file("shiny-examples", package = "esapriorities")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing 'mypackage'.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing 'esapriorities'.", call. = FALSE)
   }
   shiny::runApp(appDir, display.mode = "normal")
 }
